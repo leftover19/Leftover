0f15d89d91827fa8c036662e6e62ec184df95c0b
leftover19 <vishwaskapoor27@gmail.com>
2024-03-19 11:24:28 +0530

    added express and cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,14 +1,16 @@
 import http from 'http'
+import express from 'express';
+import cors from 'cors';
 import SocketService from './services/socket';
 import { startMessageConsumer } from './services/kafka';
 async function init() {
+    const app = express();
+
+    app.use(cors());
+    const httpServer = http.createServer(app);
+
     startMessageConsumer();
     const socketService = new SocketService();
-    const httpServer = http.createServer((req, res) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'POST , PUT , DELETE  , OPTIONS, GET');
-        res.setHeader('Access-Control-Max-Age', 60 * 60 * 24 * 30);
-    });
 
 
     const PORT = process.env.PORT ? process.env.PORT : 8000;
@@ -17,7 +19,6 @@ async function init() {
 
     httpServer.listen(PORT, () => console.log(`Http Server Up & Running at PORT:${PORT}`));
     socketService.initListeners();
-
 }
 
 init();
