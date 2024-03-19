dc78a126acc6b1913521913ffc79046eae04a63d
leftover19 <vishwaskapoor27@gmail.com>
2024-03-19 10:19:22 +0530

    added * in cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -7,14 +7,17 @@ async function init() {
     const httpServer = http.createServer();
 
     httpServer.on('request', (req, res) => {
-        // Allow requests only from the specified origin
         res.setHeader('Access-Control-Allow-Origin', '*');
-
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         if (req.method === 'OPTIONS') {
             res.writeHead(200);
             res.end();
             return;
         }
+
+        // Proceed to your regular request handling
+        // ...
     });
     const PORT = process.env.PORT ? process.env.PORT : 8000;
 
