685043b4f378b611490e1d611968cf7e860afdb4
leftover19 <vishwaskapoor27@gmail.com>
2024-03-19 11:08:42 +0530

    server-fix

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,21 +4,13 @@ import { startMessageConsumer } from './services/kafka';
 async function init() {
     startMessageConsumer();
     const socketService = new SocketService();
-    const httpServer = http.createServer();
-
-    httpServer.on('request', (req, res) => {
+    const httpServer = http.createServer((req, res) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        if (req.method === 'OPTIONS') {
-            res.writeHead(200);
-            res.end();
-            return;
-        }
-
-        // Proceed to your regular request handling
-        // ...
+        res.setHeader('Access-Control-Allow-Methods', 'POST , PUT , DELETE  , OPTIONS, GET');
+        res.setHeader('Access-Control-Max-Age', 60 * 60 * 24 * 30);
     });
+
+
     const PORT = process.env.PORT ? process.env.PORT : 8000;
 
     socketService.io.attach(httpServer);
